533d4bfeb43689fd9331fb10f5625e5e54b1a2c8
Noah Bresler <noahbresler2002@gmail.com>
2022-04-17 00:26:04 -0400

    Implement dataset ingestion

--- solver/main.cpp
+++ solver/main.cpp
@@ -1,7 +1,13 @@
+#include <chrono>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
 
 #include "AdjacencyList.hpp"
+#include "BellmanFord.cpp"
+
+using namespace std::chrono;
 
 enum GraphSample {
   // To be implemented
@@ -12,7 +18,7 @@ enum GraphSample {
   // Randomly generated graphs
   RandomGraph
 };
-GraphSample graphToUse = RandomGraph;
+GraphSample graphToUse = DataSet1;
 int randomGraphCount = 50;
 unsigned int randomWeightMax = 50;
 
@@ -20,7 +26,7 @@ int main() {
   AdjacencyList<unsigned int> graph;
 
   switch (graphToUse) {
-  case RandomGraph:
+  case RandomGraph: {
     for (int i = 0; i < randomGraphCount; i++) {
       auto edge1 = std::rand() % randomGraphCount;
       auto edge2 = std::rand() % randomGraphCount;
@@ -30,10 +36,48 @@ int main() {
       graph.insertEdge(edge1, edge2, weight);
       graph.insertEdge(edge2, edge1, weight);
     }
+
     break;
+  }
+  case DataSet1: {
+    auto start = high_resolution_clock::now();
+
+    ifstream dataset("./data/roadNet-CA.txt");
+
+    // Skip first 4 lines
+    for (int i = 0; i < 4; i++)
+      dataset.ignore(numeric_limits<streamsize>::max(), dataset.widen('\n'));
+
+    string delimeter = "	";
+
+    if (dataset.is_open()) {
+      string line;
+      while (getline(dataset, line)) {
+        int from, to;
+
+        auto found = line.find(delimeter);
+        string first = line.substr(0, found);
+        from = stoi(first);
+        line.erase(0, found + delimeter.length());
+        to = stoi(line);
+
+        graph.insertEdge(from, to, std::rand() & randomWeightMax);
+      }
+
+      dataset.close();
+    }
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    cout << "Ingesting dataset took: " << (duration.count() / 1000.0)
+         << " seconds" << endl;
+
+    break;
+  }
   default:
     break;
   }
 
-  graph.printGraph(true);
+  // graph.printGraph(true);
 }
