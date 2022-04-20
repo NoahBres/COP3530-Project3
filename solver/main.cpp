5005fe2a021b14ce77fb0d6f4aa7080fb660e49e
Noah Bresler <noahbresler2002@gmail.com>
2022-04-20 00:38:36 -0400

    Unify search methods

--- solver/main.cpp
+++ solver/main.cpp
@@ -19,6 +19,8 @@ int main() {
   AdjacencyList<int> caliGraph;
 
   cout << "Reading California roads..." << endl;
+
+  // Start data ingestion timer
   auto start = high_resolution_clock::now();
   ifstream caliRoads("../data/cal-road-edges.txt");
 
@@ -27,7 +29,7 @@ int main() {
     return 0;
   }
 
-  // Scope this process just cuz
+  // Scope the file processing section
   {
     string line;
     string delimeter = " ";
@@ -35,6 +37,8 @@ int main() {
       int from, to;
       int weight;
 
+      // Read line by line. Split via spaced delimeters
+      // Parse to, found, and weight
       auto found = line.find(delimeter);
       line.erase(0, found + delimeter.length());
       found = line.find(delimeter);
@@ -45,6 +49,7 @@ int main() {
       line.erase(0, found + delimeter.length());
       weight = (int)(stod(line) * 1000000);
 
+      // Insert reverse edges as our graph is directed
       caliGraph.insertEdge(from, to, weight);
       caliGraph.insertEdge(to, from, weight);
     }
@@ -58,11 +63,14 @@ int main() {
   cout << "Building California graph took " << (duration.count() / 1000.0)
        << " seconds" << endl;
 
+  // Search REST endpoint
+  // Handles Dijkstra and Bellman Ford search requests from the client
   server.Get("/search", [&caliGraph](const httplib::Request &req,
                                      httplib::Response &res) {
-    // res.set_content("Hey", "text/plain");
+    //  Solve CORS issue
     res.set_header("Access-Control-Allow-Origin", "*");
 
+    // Check if params are valid
     if (!req.has_param("from") || !req.has_param("to") ||
         !req.has_param("type")) {
       res.set_content("Request requires from, to, and type", "text/plain");
@@ -71,88 +79,56 @@ int main() {
       auto to = stoi(req.get_param_value("to"));
       auto type = req.get_param_value("type");
 
+      // Handle dijkstra and bellman ford searches separately
+      string cacheReadFilePath = "";
+      pair<int, stack<int>> (*searchMethod)(AdjacencyList<int>, int, int);
+
+      stack<int> pathStack;
+      int distance;
+
+      // Switch between search methods based on request
       if (type == "dijkstra") {
-        auto cacheReadOut = cacheRead(from, to, "cache_dijkstra");
-
-        auto start = high_resolution_clock::now();
-
-        stack<int> pathStack;
-        int distance;
-
-        cout << cacheReadOut.first << endl;
-        if (cacheReadOut.first != INT_MAX) {
-          cout << "Result retrieved from cache" << endl;
-
-          distance = cacheReadOut.first;
-          pathStack = cacheReadOut.second;
-        } else {
-          cout << "Starting Dijkstra search..." << endl;
-
-          auto solution = Dijkstra(caliGraph, from, to);
-          distance = solution.first;
-          pathStack = solution.second;
-
-          cacheWrite(from, to, solution.first, solution.second,
-                     "cache_dijkstra");
-        }
-
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<milliseconds>(stop - start);
-
-        string path = "";
-        while (!pathStack.empty()) {
-          path += to_string(pathStack.top());
-          path += ",";
-          pathStack.pop();
-        }
-
-        res.set_content(
-            "{\"path\":\"" + path + "\", \"distance\": " + to_string(distance) +
-                ", \"time\": " + to_string(duration.count()) + " " + "}",
-            "application/json");
+        cacheReadFilePath = "cache_dijkstra";
+        searchMethod = &Dijkstra;
       } else if (type == "bellmanford") {
-        auto cacheReadOut = cacheRead(from, to, "cache_bellman");
-
-        auto start = high_resolution_clock::now();
-
-        stack<int> pathStack;
-        int distance;
-
-        cout << cacheReadOut.first << endl;
-        if (cacheReadOut.first != INT_MAX) {
-          cout << "Result retrieved from cache" << endl;
-
-          distance = cacheReadOut.first;
-          pathStack = cacheReadOut.second;
-        } else {
-          cout << "Starting Bellman Ford search..." << endl;
-
-          auto solution = BellmanFord(caliGraph, from, to);
-          distance = solution.first;
-          pathStack = solution.second;
-
-          cacheWrite(from, to, solution.first, solution.second,
-                     "cache_bellman");
-        }
-
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<milliseconds>(stop - start);
-
-        string path = "";
-        while (!pathStack.empty()) {
-          path += to_string(pathStack.top());
-          path += ",";
-          pathStack.pop();
-        }
-
-        res.set_content(
-            "{\"path\":\"" + path + "\", \"distance\": " + to_string(distance) +
-                ", \"time\": " + to_string(duration.count()) + " " + "}",
-            "application/json");
-
-      } else {
-        res.set_content("Unsupported type", "text/plain");
+        cacheReadFilePath = "cache_bellman";
+        searchMethod = &BellmanFord;
       }
+
+      auto cacheReadOut = cacheRead(from, to, cacheReadFilePath);
+
+      auto start = high_resolution_clock::now();
+
+      if (cacheReadOut.first != INT_MAX) {
+        cout << "Result retrieved from cache" << endl;
+
+        distance = cacheReadOut.first;
+        pathStack = cacheReadOut.second;
+      } else {
+        cout << "Starting search..." << endl;
+
+        auto solution = searchMethod(caliGraph, from, to);
+        distance = solution.first;
+        pathStack = solution.second;
+
+        cacheWrite(from, to, solution.first, solution.second,
+                   cacheReadFilePath);
+      }
+
+      auto stop = high_resolution_clock::now();
+      auto duration = duration_cast<milliseconds>(stop - start);
+
+      string path = "";
+      while (!pathStack.empty()) {
+        path += to_string(pathStack.top());
+        path += ",";
+        pathStack.pop();
+      }
+
+      res.set_content(
+          "{\"path\":\"" + path + "\", \"distance\": " + to_string(distance) +
+              ", \"time\": " + to_string(duration.count()) + " " + "}",
+          "application/json");
     }
   });
 
